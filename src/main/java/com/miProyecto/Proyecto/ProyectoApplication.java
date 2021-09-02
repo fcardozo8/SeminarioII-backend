4ecd2fe29e52e11b7569cb5a5072a43652d3eaf9
Franco <franco.racing.cardozo@gmail.com>
2021-09-02 04:30:51 -0300

    SeminarioBackend [Edit ProyectoApplication]

--- src/main/java/com/miProyecto/Proyecto/ProyectoApplication.java
+++ src/main/java/com/miProyecto/Proyecto/ProyectoApplication.java
@@ -2,12 +2,17 @@ package com.miProyecto.Proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/SeminarioBackend")
 @SpringBootApplication
+@RestController
 public class ProyectoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
 	}
+
 
 }
